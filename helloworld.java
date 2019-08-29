ef787c5b540941d2d98f6f7070efa5967f606425
Cloud User <ec2-user@ip-172-31-87-33.ec2.internal>
2019-08-29 22:14:28 +0000

    main

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,10 @@
 /*This is a simple java program by Bir Rana */
 
-class helloworld {
-public static void main(String args[]) 
+class helloworld 
+{
+
+	public static void main(String args[]) 
+
     { 
         System.out.println("Hello, World"); 
     } 
